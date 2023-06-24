177b666487195494189ff5d969845e884ac35cf5
Samantha <contact@samanthanguyen.me>
2023-06-24 01:58:37 -0500

    feat: add icon for Bill component

--- src/components/Bill.tsx
+++ src/components/Bill.tsx
@@ -1,3 +1,5 @@
+import { Page } from "iconoir-react";
+
 import { Card, CardProps } from "./Card";
 
 export type BillType = 
@@ -16,8 +18,11 @@ export type BillProps = CardProps & {
 
 export const Bill = ({uniqueId, type, desc, ...props}: BillProps) => {
 	return (
-		<Card className="gap-2 bg-slate-50 border border-slate-200">
-			<div>
+		<Card className="flex-row gap-3 items-start px-4 bg-slate-50 border border-slate-200" {...props}>
+			<div className="bg-slate-200 p-3 rounded-lg">
+				<Page />
+			</div>
+			<div className="flex flex-col gap-1">
 				<header className="flex flex-row gap-2 items-center">
 					<span className="text-lg">{uniqueId}</span>
 					<span className="text-slate-500 text-m">{type}</span>
